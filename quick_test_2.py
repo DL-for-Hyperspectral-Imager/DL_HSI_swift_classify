db7ec304d53996990c5aac80f5744bb8c7a742c8
HF-biu <1564598834@qq.com>
2023-05-14 15:55:34 +0800

    pictures

--- quick_test_2.py
+++ quick_test_2.py
@@ -3,16 +3,71 @@
 import subprocess
 import argparse
 import main
+import matplotlib.pyplot as plt
 
-model = 'nearest'
-preprocess_list = ['pca', 'ica', 'lda']
-n_bands_list    = [25, 50, 75, 100, 125, 150, 175]
+model_list = ['svm','nearest']# 'nn'
+preprocess_list = ['pca', 'ica']
+n_bands_list    = [25, 50, 75, 100, 125, 150, 175,200]
 
+for model in model_list:
 # 以不同的波段数和降维方法进行多次测试
-for preprocess in preprocess_list:
+    for preprocess in preprocess_list:
+        train_times = []
+        predict_times = []
+        accuracys = []
+        for n_bands in n_bands_list:
+            # run_results, Training_time, Predicting_time即为本次运行的结果
+            # run_results中包含了accuracy, F1 score by class, confusion matrix,为字典
+            run_results, Training_time, Predicting_time = main.main(False, n_runs = 1, dataset = 'IndianPines', preprocess = preprocess, model = model, training_sample = 0.1,\
+                                     n_bands = n_bands, img_path = 'result')
+            # 记录数据，可以增加其他属性
+            accuracys.append(run_results['Accuracy'])
+            train_times.append( Training_time)
+            predict_times.append(Predicting_time)
+        #绘图
+        fig = plt.figure(figsize = (10,4))
+        plt.subplot(121)
+        plt.title("                                    For model of "+model+" and preprocess of "+preprocess)
+        plt.xlabel('bands')
+        plt.ylabel('accuracy/%')
+        plt.plot(n_bands_list,accuracys,"og-")
+        plt.subplot(122)
+        plt.xlabel('bands')
+        plt.ylabel('time/s')
+        plt.plot(n_bands_list,train_times,"ob-",label = "train time")
+        plt.plot(n_bands_list,predict_times,"or-",label = "predict time")
+        plt.legend()
+        plt.savefig("./result/" + model + "_" + preprocess + ".jpg")
+        plt.show()
+      
+preprocess = 'lda' #lda需要用不同的bands
+
+n_bands_list    = [1,3,5,7,9,11,13,15]
+for model in model_list:
+    train_times = []
+    predict_times = []
+    accuracys = []
     for n_bands in n_bands_list:
         # run_results, Training_time, Predicting_time即为本次运行的结果
         # run_results中包含了accuracy, F1 score by class, confusion matrix,为字典
         run_results, Training_time, Predicting_time = main.main(False, n_runs = 1, dataset = 'IndianPines', preprocess = preprocess, model = model, training_sample = 0.1,\
                                  n_bands = n_bands, img_path = 'result')
-        # 下面进行分析处理...
+        # 记录数据，可以增加其他属性
+        accuracys.append(run_results['Accuracy'])
+        train_times.append( Training_time)
+        predict_times.append(Predicting_time)
+    #绘图
+    fig = plt.figure(figsize = (10,4))
+    plt.subplot(121)
+    plt.title("                                    For model of "+model+" and preprocess of "+preprocess)
+    plt.xlabel('bands')
+    plt.ylabel('accuracy/%')
+    plt.plot(n_bands_list,accuracys,"og-")
+    plt.subplot(122)
+    plt.xlabel('bands')
+    plt.ylabel('time/s')
+    plt.plot(n_bands_list,train_times,"ob-",label = "train time")
+    plt.plot(n_bands_list,predict_times,"or-",label = "predict time")
+    plt.legend()
+    plt.savefig("./result/" + model + "_" + preprocess + ".jpg")
+    plt.show()
